2b15cd848fee5e7897eb661eb20a713de7b6a34a
Poiza <poiza@list.ru>
2020-10-23 14:48:06 +0300

    TDG-12 add repo_id to Note

--- note/__init__.py
+++ note/__init__.py
@@ -7,9 +7,10 @@ from datetime import date
 class Note:
     """Note implementation."""
 
-    def __init__(self, user_id, text):
+    def __init__(self, user_id, text, repo_id = 0):
         self.user_id = user_id
         self.text = text
+        self.repo_id = repo_id
         self.date = str(date.today())
 
     def __str__(self):
