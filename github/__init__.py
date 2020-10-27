7dc6866aa8758a2df63b6fdc6009aafa4e27bf8f
Poiza <poiza@list.ru>
2020-10-27 12:00:01 +0300

    TDG-5 correct initialization

--- github/__init__.py
+++ github/__init__.py
@@ -14,11 +14,18 @@ class Repository:
         self.owner = owner
         self.repo_name = repo_name
         self.dir = None
+        self._repo = None
         self.clone()
 
     @property
     def url(self):
         return GH_URL + "/{0.owner}/{0.repo_name}.git".format(self)
+
+    @property
+    def git_dir(self):
+        if self._repo is None:
+            self._repo = Git(self.dir)
+        return self._repo
 
     def clone(self):
         """Clone github repository to a temporary dir"""
@@ -27,15 +34,14 @@ class Repository:
             # TODO update repo
         else:
             tmp_dir = mkdtemp()
-            Git(tmp_dir).clone(self.url, "--bare")
+            Git(tmp_dir).clone(self.url, "--bare")  # It's not a _repo !!!
             self.dir = os.path.join(tmp_dir, self.repo_name+".git")
 
     def get_file_content(self, filepath, branch="master"):
         """Return file content"""
         try:
-            repo = Git(self.dir)
             filename = "{}:{}".format(branch, filepath)
-            content = repo.show(filename)
+            content = self.git_dir.show(filename)
         except BaseException:
             pass
             # TODO idk how to handle this exception
